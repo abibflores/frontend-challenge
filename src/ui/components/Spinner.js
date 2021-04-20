33dda714f13fde307c45b4a915a4e89d761fbb81
AbibF <gonzalo.moreno@televisatim.com>
2021-04-19 23:08:55 -0500

    feat(ux):  Add Spinner

---
+++ src/ui/components/Spinner.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Spinner = () => (
+    <div className="Spinner">
+        <div className="Spinner__Circle">
+            <div />
+        </div>
+        <span>Cargando ofertas ...</span>
+    </div>
+);
